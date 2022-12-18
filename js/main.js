6bce6e1717e4001b06b2259f614ef0b730ae4fad
Voitko Viacheslav <voitko-viacheslav-ivanovich1992@ukr.net>
2022-12-18 12:35:22 +0200

    'feat: add minifier, optimizer img, scroll for links'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// HEADER
 (function () {
   const header = document.querySelector('.header__wrapper');
   window.onscroll = () => {
@@ -7,16 +8,75 @@
       header.classList.remove('header_active')
     }
   }
-}())
+}());
 
+
+// BURGER
 (function () {
   const burderItem = document.querySelector('.burger');
   const menu = document.querySelector('.header__nav');
   const menuCloseItem = document.querySelector('.header__nav-close')
+  const menuLinks = document.querySelectorAll('.header__link ');
   burderItem.addEventListener('click', () => {
     menu.classList.add('header__nav-active')
-  })
+  });
   menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('header__nav-active')
-  })
+  });
+  if(window.innerWidth <= 767) {
+    for (let i = 0; i < menuLinks.length; i++) {
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove('header__nav-active')
+      })
+    }
+    
+  }
+  
+
+
 }());
+
+
+// SCROLL
+
+
+(function () {
+
+  const smoothScroll = function (targetEl, duration) {
+      const headerElHeight =  document.querySelector('.header').clientHeight;
+      let target = document.querySelector(targetEl);
+      let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+      let startPosition = window.pageYOffset;
+      let startTime = null;
+  
+      const ease = function(t,b,c,d) {
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+  
+      const animation = function(currentTime){
+          if (startTime === null) startTime = currentTime;
+          const timeElapsed = currentTime - startTime;
+          const run = ease(timeElapsed, startPosition, targetPosition, duration);
+          window.scrollTo(0,run);
+          if (timeElapsed < duration) requestAnimationFrame(animation);
+      };
+      requestAnimationFrame(animation);
+
+  };
+
+  const scrollTo = function () {
+      const links = document.querySelectorAll('.js-scroll');
+      links.forEach(each => {
+          each.addEventListener('click', function () {
+              const currentTarget = this.getAttribute('href');
+              smoothScroll(currentTarget, 1300);
+          });
+      });
+  };
+  scrollTo();
+}());
+
+
